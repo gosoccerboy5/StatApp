5b9bb63392cdf318a536c05dad7887b5162653a9
gosoccerboy5 <gosoccerboy6@gmail.com>
2024-04-23 12:04:23 -0500

    Residuals and Transformations (#6)
    * add residuals and transformations
    
    * Update regression.html

--- regression.js
+++ regression.js
@@ -46,12 +46,26 @@ addDataRow();
 addDataRow();
 document.activeElement.blur();
 
+$("#transformation").addEventListener("input", function(e) {
+  if (e.data === " ") {
+    this.value = this.value.replaceAll(/\W/g, "");
+  }
+});
+addEnterEvent($("#transformation"), update);
+function transform(number) {
+  let transformation = $("#transformation").value || $("#transformation").placeholder;
+  try {
+    return evaluate(transformation.replace(/\by\b/g, number));
+  } catch {return null;}
+}
+
 function getData() {
   let pairs = [];
   for (let i = 1; i < $("#data").children.length; i++) {
     let pair = [...$("#data").children[i].children].map(el => el.children[0].value);
-    if (pair.every(n => /^-?(\d*\.)?\d+$/.test(n))) {
-      pairs.push(pair.map(Number));
+    let y = transform(pair[1]);
+    if (y !== null && pair.every(n => /^-?(\d*\.)?\d+$/.test(n))) {
+      pairs.push([pair[0], y]);
     }
   }
   return pairs;
@@ -61,6 +75,7 @@ function saveData() {
   localStorage.setItem("confidence", $("#confidence").value);
   localStorage.setItem("x", [...$("#data").children].map(el => el.children[0].children[0].value));
   localStorage.setItem("y", [...$("#data").children].map(el => el.children[1].children[0].value));
+  localStorage.setItem("transform", $("#transformation").value);
 }
 $("#save").addEventListener("click", saveData);
 function populate() {
@@ -74,6 +89,7 @@ function populate() {
       el.children[1].children[0].value = el.children[1].children[0].lastValue = localStorage.getItem("y").split(",")[idx];
     });
   }
+  if (localStorage.getItem("transform") !== null) $("#transformation").value = localStorage.getItem("transform");
 }
 $("#load").addEventListener("click", populate);
 
@@ -233,19 +249,25 @@ function convert(str) {
 
 function updatePage(output) {
   $("#output").style.display = "";
+  let transformation = $("#transformation").value || $("#transformation").placeholder;
   let xName = $("#xName").value || "x", yName = $("#yName").value || "y";
+  let transY = transformation.replace(/\by\b/g, yName), transConvertedY = transformation.replace(/\by\b/g, convert(yName));
   let confidence = Math.min((Number($("#confidence").value) || 95), 99)/100;
   let show = ["#showpoints", "#showline", "#showconfidence", "#showprediction"].map(id => $(id).checked);
   window.output = output;
   let m = 50;
   let canvas = document.querySelector("#canvas");
   let ctx = canvas.getContext("2d");
+  let residCanvas = $("#residualPlot");
+  let rctx = residCanvas.getContext("2d");
   ctx.fillStyle = getComputedStyle(document.body).backgroundColor;
-  ctx.textAlign = "center";
+  ctx.textAlign = rctx.textAlign = "center";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  rctx.fillStyle = getComputedStyle(document.body).backgroundColor;
+  rctx.fillRect(0, 0, residCanvas.width, residCanvas.height);
   ctx.fillStyle = "black";
   ctx.font = "20px Times";
-  ctx.fillText(`${convert(yName)} vs ${convert(xName)}`, canvas.width/2, m-10);
+  ctx.fillText(`${transConvertedY} vs ${convert(xName)}`, canvas.width/2, m-10);
   ctx.lineWidth = 2;
   line(ctx, m, m, m, canvas.height-m);
   line(ctx, m, canvas.height-m, canvas.width-m, canvas.height-m);
@@ -270,7 +292,7 @@ function updatePage(output) {
     drawRotatedText(ctx, trunc((canvas.height-m-i)*maxY/(canvas.height-2*m)), 2*m/3, i, 3*Math.PI/2);
   }
   ctx.font = "20px Times";
-  drawRotatedText(ctx, convert(yName), m/3, canvas.height/2, 3*Math.PI/2);
+  drawRotatedText(ctx, transConvertedY, m/3, canvas.height/2, 3*Math.PI/2);
   ctx.lineWidth = 2;
   let translate = (x, y) => [m+(canvas.width-2*m)/maxX*x, canvas.height-m-(canvas.height-2*m)/maxY*y];
   ctx.fillStyle = "black";
@@ -291,15 +313,40 @@ function updatePage(output) {
     lastLine = prediction;
   }
 
+  rctx.lineWidth = 2;
+  rctx.fillStyle = "black";
+  line(rctx, m, residCanvas.height/2, residCanvas.width-m, residCanvas.height/2);
+  line(rctx, m, m, m, residCanvas.height-m);
+  let residuals = output.data.toSorted((a, b) => a[0]-b[0]).map(pair => [pair[0], pair[1]-output.predict(pair[0])]);
+  let standardizedResiduals = residuals.map(pair => [pair[0], output.sResid < 1e-6 ? 0 : pair[1]/output.sResid]);
+  let maxResid = output.sResid < 1e-6 ? 1 : Math.floor(standardizedResiduals.reduce((a, b) => Math.max(a, Math.abs(b[1])), 0)+1);
+  let translateResid = (x, y) => [m+x/maxX*(residCanvas.width-m*2), canvas.height/2-(canvas.height-2*m)/(2*maxResid)*y];
+  rctx.font = "20px Times";
+  drawRotatedText(rctx, "Standardized Residuals", m/4, residCanvas.height/2, Math.PI*3/2);
+  rctx.fillText(convert(xName), residCanvas.width/2, residCanvas.height-m/2);
+  rctx.fillText(convert(xName) + " Residual Plot", residCanvas.width/2, m/2);
+  for (let i = maxResid; i >= -maxResid; i--) {
+    line(rctx, m*3/4, translateResid(0, i)[1], m*1.25, translateResid(0, i)[1]);
+    rctx.font = "12px Arial";
+    rctx.fillText(i, m/2, translateResid(0, i)[1]+4);
+  }
+  for (let i = (residCanvas.width-2*m)*xStep/maxX; i <= (residCanvas.width-2*m); i+=(residCanvas.width-2*m)*xStep/maxX) {
+    line(rctx, i+m, residCanvas.height/2-m/4, i+m, residCanvas.height/2+m/4);
+    rctx.font = "12px Arial";
+    rctx.fillText(trunc(i*maxX/(canvas.width-2*m)), i+m, residCanvas.height/2+m*1/2);
+  }
+  for (let pair of standardizedResiduals) {
+    circle(rctx, ...translateResid(pair[0], pair[1]), 3);
+  }
+
   $("#count").innerText = `n = ${output.n}; df = ${output.n-2}`;
-  $("#equation").innerText = `${yName} = ${trunc(output.a)} ${output.b<0 ? "–" : "+"} ${trunc(Math.abs(output.b))}${xName}`;
+  $("#equation").innerText = `${transY} = ${trunc(output.a)} ${output.b<0 ? "–" : "+"} ${trunc(Math.abs(output.b))}${xName}`;
   $("#r").innerText = `r = ${trunc(output.r)}`;
   $("#r2").innerText = `R² = ${trunc(output.r**2*100, 3)}%`;
   $("#slopeT").innerHTML = `t<sub>b</sub> = ${trunc(output.tStatistic)}; p-value = ${trunc(2*tCDF(output.tStatistic, null, output.n-2))}`;
   let error = invT(confidence, output.n-2, 0) * output.b/output.tStatistic;
   $("#slopeInterval").innerText = `${confidence*100}% CI for b: ${trunc(output.b)} ± ${trunc(error)} = (${trunc(output.b-error)}, ${trunc(output.b+error)})`;
-  let residuals = output.data.toSorted((a, b) => a[0]-b[0]).map(pair => [pair[0], pair[1]-output.predict(pair[0])]);
-  $("#residuals").innerText = `Residuals: {${residuals.map(pair => pair[0] + ": " + trunc(pair[1])).join(", ")}}`;
+  //$("#residuals").innerText = `Residuals: {${residuals.map(pair => pair[0] + ": " + trunc(pair[1])).join(", ")}}`;
   $("#sResid").innerHTML = `S<sub>resid</sub> = ${trunc(output.sResid)}; x̄ = ${trunc(output.meanX)}; ȳ = ${trunc(output.meanY)}; S<sub>x</sub> = ${trunc(output.sdX)};
   S<sub>y</sub> = ${trunc(output.sdY)}; SS<sub>x</sub> = ${trunc(output.ssX)}; SS<sub>y</sub> = ${trunc(output.ssY)}`;
 
@@ -307,7 +354,7 @@ function updatePage(output) {
   function predictX() {
     let x = Number($("#predictX").value || 0);
     let prediction = output.predict(x, confidence);
-    $("#outputPredictX").innerText = `ŷ(${x}) = ${trunc(prediction[0])}\n${confidence*100}% CI = ${trunc(prediction[0])} ± ${trunc(prediction[2]-prediction[0])} =` +
+    $("#outputPredictX").innerText = `${transY} = ${trunc(prediction[0])}\n${confidence*100}% CI = ${trunc(prediction[0])} ± ${trunc(prediction[2]-prediction[0])} =` +
     `(${trunc(prediction[1])}, ${trunc(prediction[2])})\n${confidence*100}% PI = ${trunc(prediction[0])} ± ${trunc(prediction[4]-prediction[0])} =` +
     `(${trunc(prediction[3])}, ${trunc(prediction[4])})`;
   }
@@ -317,13 +364,10 @@ function updatePage(output) {
   function predictY() {
     let y = Number($("#predictY").value || 0);
     let prediction = output.inversePredict(y, confidence);
-    $("#outputPredictY").innerText = `x̂(${y}) = ${trunc(prediction[0])}\n${confidence*100}% CI = ${trunc(prediction[0])} ± ${trunc(prediction[2]-prediction[0])} =` +
+    $("#outputPredictY").innerText = `${xName} = ${trunc(prediction[0])}\n${confidence*100}% CI = ${trunc(prediction[0])} ± ${trunc(prediction[2]-prediction[0])} =` +
     `(${trunc(prediction[1])}, ${trunc(prediction[2])})\n${confidence*100}% PI = ${trunc(prediction[0])} ± ${trunc(prediction[4]-prediction[0])} =` +
     `(${trunc(prediction[3])}, ${trunc(prediction[4])})`;
   }
   $("#enterPredictY").addEventListener("click", predictY);
   addEnterEvent($("#predictY"), predictY);
 }
-//updatePage(regress([[1, 1], [2, 3], [3, 4], [4, 5], [5, 5], [7, 6], [8, 7], [9, 8]]));
-//updatePage(regress([[0, .02778], [.5, .05], [.625, 0], [.75, .20833], [1, .2], [1.25, .5], [1.5, .15], [1.75, .625], [2, .42857], [2.25, .875], [2.5, 1], [3, 1]]));
-//updatePage(regress([[17, 119.4792], [18, 130.3979], [19, 122.6063], [20, 131.4479], [21, 130.2792], [22, 131.5556], [23, 128.0833], [24, 131.6319], [25, 126.7722], [27, 127.7938], [28, 129.3910], [29, 121.4271], [30, 127.8125], [31, 119.5882], [32, 134.5639], [33, 120.5403], [34, 131.8361], [35, 125.8431], [36, 118.5597]]));
